5c3c151d1743db6fdc3806929968c5de0f5e3525
yorickdewid <yorick17@outlook.com>
2017-03-26 23:30:15 +0200

    Listen on socket

--- EntropyServer/Main.cpp
+++ EntropyServer/Main.cpp
@@ -17,77 +17,157 @@
 #include <wx/timer.h>
 #include <wx/thread.h>
 
-class Daemon : public wxApp {
-	//wxDECLARE_EVENT_TABLE();
+class Daemon : public wxApp
+{
+	wxDECLARE_EVENT_TABLE();
+
+	unsigned short m_port = 52630;
+
+	wxSocketServer *m_listeningSocket;
+
 public:
 	Daemon() {}
 	~Daemon() {}
 
-	virtual bool OnInit() wxOVERRIDE {
-		if (!wxApp::OnInit())
-			return false;
+	virtual bool OnInit() wxOVERRIDE;
+	virtual int OnExit() wxOVERRIDE;
 
-		wxLogMessage("I am here");
+	void OnInitCmdLine(wxCmdLineParser& parser);
+	bool OnCmdLineParsed(wxCmdLineParser& pParser);
 
-		return true;
-	}
-
-	virtual int OnExit() wxOVERRIDE {
-		wxLogMessage("Bye");
-
-		return 0;
-	}
-
-	void OnInitCmdLine(wxCmdLineParser& parser)
-	{
-		wxApp::OnInitCmdLine(parser);
-		parser.SetLogo("Cryptox Entropy Server - Copyright (c) 2017");
-		parser.AddSwitch("a", "auth", "request authentication");
-		parser.AddOption("p", "port", "listen on given port (default 52630)", wxCMD_LINE_VAL_NUMBER);
-		parser.AddSwitch("v", "version", "show version and exit");
-	}
-
-	bool OnCmdLineParsed(wxCmdLineParser& pParser)
-	{
-		if (pParser.Found("verbose"))
-		{
-			wxLog::AddTraceMask("wxSocket");
-			wxLog::AddTraceMask("epolldispatcher");
-			wxLog::AddTraceMask("selectdispatcher");
-			wxLog::AddTraceMask("thread");
-			wxLog::AddTraceMask("events");
-			wxLog::AddTraceMask("timer");
-		}
-#if 0
-		if (pParser.Found("m", &m_maxConnections))
-		{
-			wxLogMessage("%ld connection(s) to exit", m_maxConnections);
-		}
-
-		long port;
-		if (pParser.Found("p", &port))
-		{
-			if (port <= 0 || port > USHRT_MAX)
-			{
-				wxLogError("Invalid port number %ld, must be in 0..%u range.",
-					port, USHRT_MAX);
-				return false;
-			}
-
-			m_port = static_cast<unsigned short>(port);
-			wxLogMessage("Will listen on port %u", m_port);
-		}
-
-		if (pParser.Found("t"))
-			m_workMode = THREADS;
-		else if (pParser.Found("e"))
-			m_workMode = EVENTS;
-		else
-			m_workMode = MIXED;
-#endif
-		return wxApp::OnCmdLineParsed(pParser);
-	}
+	void OnSocketEvent(wxSocketEvent& evt);
 };
 
 wxDECLARE_APP(Daemon);
 wxIMPLEMENT_APP_CONSOLE(Daemon);
+
+wxBEGIN_EVENT_TABLE(Daemon, wxEvtHandler)
+	EVT_SOCKET(wxID_ANY, Daemon::OnSocketEvent)
+	//EVT_WORKER(Daemon::OnWorkerEvent)
+	//EVT_TIMER(wxID_ANY, Daemon::OnTimerEvent)
+wxEND_EVENT_TABLE()
+
+
+void Daemon::OnInitCmdLine(wxCmdLineParser& parser)
+{
+	wxApp::OnInitCmdLine(parser);
+	parser.SetLogo("Cryptox Entropy Server - Copyright (c) 2017");
+	parser.AddSwitch("a", "auth", "request authentication");
+	parser.AddOption("p", "port", "listen on given port (default 52630)", wxCMD_LINE_VAL_NUMBER);
+	parser.AddSwitch("v", "version", "show version and exit");
+}
+
+
+bool Daemon::OnCmdLineParsed(wxCmdLineParser& pParser)
+{
+	wxLogMessage("Parsing arguments");
+
+	if (pParser.Found("verbose"))
+	{
+		wxLog::AddTraceMask("wxSocket");
+		wxLog::AddTraceMask("epolldispatcher");
+		wxLog::AddTraceMask("selectdispatcher");
+		wxLog::AddTraceMask("thread");
+		wxLog::AddTraceMask("events");
+		wxLog::AddTraceMask("timer");
+	}
+
+	return wxApp::OnCmdLineParsed(pParser);
+}
+
+
+bool Daemon::OnInit() {
+	if (!wxApp::OnInit())
+		return false;
+
+	wxLogMessage("I am here");
+
+	wxIPV4address addr;
+	addr.Service(m_port);
+
+	m_listeningSocket = new wxSocketServer(addr, wxSOCKET_NOWAIT | wxSOCKET_REUSEADDR);
+	m_listeningSocket->SetEventHandler(*this);
+	m_listeningSocket->SetNotify(wxSOCKET_CONNECTION_FLAG);
+	m_listeningSocket->Notify(true);
+
+	if (!m_listeningSocket->IsOk())
+	{
+		wxLogError("Cannot bind listening socket");
+		return false;
+	}
+
+	return true;
+}
+
+
+int Daemon::OnExit() {
+	wxLogMessage("Shutdown server");
+
+	m_listeningSocket->Destroy();
+	return 0;
+
+	return 0;
+}
+
+
+void Daemon::OnSocketEvent(wxSocketEvent& evt)
+{
+	switch (evt.GetSocketEvent())
+	{
+	case wxSOCKET_INPUT:
+		wxLogError("Unexpected wxSOCKET_INPUT in wxSocketServer");
+		break;
+	case wxSOCKET_OUTPUT:
+		wxLogError("Unexpected wxSOCKET_OUTPUT in wxSocketServer");
+		break;
+	case wxSOCKET_CONNECTION:
+	{
+		auto sock = m_listeningSocket->Accept();
+		wxIPV4address addr;
+		if (!sock->GetPeer(addr))
+		{
+			wxLogError("Server: cannot get peer info");
+		}
+		else {
+			wxLogMessage("Got connection from %s:%d", addr.IPAddress().c_str(), addr.Service());
+		}
+#if 0
+		bool createThread;
+
+		if (m_workMode != MIXED)
+			createThread = m_workMode == THREADS;
+		else
+			createThread = (wxDateTime::Now().GetSecond()) % 2 == 0;
+
+		if (createThread)
+		{
+			ThreadWorker* c = new ThreadWorker(sock);
+			if (c->Create() == wxTHREAD_NO_ERROR)
+			{
+				m_threadWorkers.Append(c);
+				if (m_threadWorkers.GetCount() > m_maxThreadWorkers)
+					m_maxThreadWorkers++;
+				m_threadWorkersCreated++;
+				c->Run();
+			}
+			else
+			{
+				wxLogError("Server: cannot create next thread (current threads: %d", m_threadWorkers.size());
+			};
+		}
+		else
+		{
+			EventWorker* w = new EventWorker(sock);
+			m_eventWorkers.Append(w);
+			if (m_eventWorkers.GetCount() > m_maxEventWorkers)
+				m_maxEventWorkers++;
+			m_eventWorkersCreated++;
+		}
+#endif
+	}
+	break;
+	case wxSOCKET_LOST:
+		wxLogError("Unexpected wxSOCKET_LOST in wxSocketServer");
+		break;
+	}
+}
