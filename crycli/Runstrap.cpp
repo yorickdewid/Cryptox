85855089435c1c849a902f1cc3cb5ea4fc894ab9
yorickdewid <yorick17@outlook.com>
2017-07-23 23:16:13 +0200

    Function description

--- crycli/Runstrap.cpp
+++ crycli/Runstrap.cpp
@@ -16,13 +16,17 @@ public:
 		FileToProg();
 	}
 
+	// Create a new compiler and run the source code. The compiler is configured to
+	// be using the lexer to parse the program.
 	void Start()
 	{
 		// crycl::compiler<crycl::lexer> cl{ ss.str() };
-		// cl.Run();
+		// cl.Compile();
 	}
 
 private:
+	// Convert file to runnable program instructions. This mainly means stripping
+	// code markup, comments and empty lines.
 	void FileToProg()
 	{
 		std::ifstream src{ fname };
@@ -45,11 +49,15 @@ private:
 
 };
 
+// Check whether the provided file exists, then bootstrap the program
+// and execute the instructions.
 void RunSource(const std::string& sourceFile)
 {
+	namespace ec = boost::system;
+
 	if (!boost::filesystem::exists(sourceFile))
 	{
-		throw boost::system::system_error{ boost::system::errc::make_error_code(boost::system::errc::no_such_file_or_directory) };
+		throw ec::system_error{ ec::errc::make_error_code(ec::errc::no_such_file_or_directory) };
 	}
 
 	Runstrap rs{ sourceFile };
