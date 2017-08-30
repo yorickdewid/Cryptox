3de65d477ec64d84fbbfd53fb37b6f6bcc228b14
yorickdewid <yorick17@outlook.com>
2017-08-30 21:33:39 +0200

    Function node

--- coilcl/AST.h
+++ coilcl/AST.h
@@ -1,9 +1,68 @@
 #pragma once
 
+#include "ValueObject.h"
+
+#include <list>
+#include <memory>
+
+class ASTNode;
+
 class AST
 {
+	std::unique_ptr<ASTNode> root = nullptr;
+
 public:
 	AST();
 	~AST();
+
+	void PushNode(std::unique_ptr<ASTNode> node)
+	{
+		// Add new root if root is empty
+		if (root == nullptr) {
+			root = std::move(node);
+		}
+	}
+};
+
+class ASTNode
+{
+	std::unique_ptr<ASTNode> parent = nullptr;
+	std::list<std::unique_ptr<ASTNode>> children;
+
+public:
+	ASTNode()
+	{
+	}
+
+	inline size_t Children()
+	{
+		return children.size();
+	}
+
+	void AppendChild(std::unique_ptr<ASTNode> node)
+	{
+		auto child = std::move(node);
+		child->parent.reset(this);
+		children.push_back(std::move(child));
+	}
+
+	inline bool IsRoot() const
+	{
+		return parent == nullptr;
+	}
+};
+
+class FunctionNode : ASTNode
+{
+	std::string funcName;
+	std::unique_ptr<Value> returnType;
+	std::list<std::unique_ptr<Value>> parameters;
+
+public:
+	FunctionNode(const std::string& name)
+		: funcName{ name }
+		, returnType{ new ValueObject<void>(Value::TypeSpecifier::T_VOID) }
+	{
+	}
 };
 
--- coilcl/ASTNode.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class ASTNode
-{
-public:
-	ASTNode() {}
-	~ASTNode() {}
-};
