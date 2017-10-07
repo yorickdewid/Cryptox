242492c9ea158075806b050b20959abff2eefdb9
yorickdewid <yorick17@outlook.com>
2017-10-07 22:41:48 +0200

    Profile compiler interface

---
+++ coilcl/Profile.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+namespace Compiler
+{
+
+struct Profile
+{
+	virtual std::string ReadInput() = 0;
+	virtual bool Include(const std::string&) = 0;
+	virtual std::shared_ptr<metainfo_t> MetaInfo() = 0;
+	virtual void Error(const std::string& message, bool isFatal) = 0;
+};
+
+} // namespace Compiler
