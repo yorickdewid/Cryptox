96a9e727ba41e9f60faf5dc0d1788ea98503eb7d
yorickdewid <yorick17@outlook.com>
2017-10-07 13:53:11 +0200

    Test CIL header file

---
+++ main.cil.h
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2017 Cryptox, Blub Corp.
+ * All Rights Reserved
+ *
+ * This file is part of the Cryptox project.
+ *
+ * Content can not be copied and/or distributed without the express
+ * permission of the author.
+ */
+
+struct info {
+    int version;
+    int counter;
+};
