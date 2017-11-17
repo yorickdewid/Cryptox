79cb8b1a3f862132ac368cba74d7ed893cb433c5
yorickdewid <yorick17@outlook.com>
2017-11-17 23:18:14 +0100

    Data type testing

---
+++ crylib/data_type.cil.c
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2017 Cryptox, Blub Corp.
+ * All Rights Reserved
+ *
+ * This file is part of the Cryptox project.
+ *
+ */
+
+char c;
+signed char sc;
+unsigned char uc;
+
+short _s;
+short int _si;
+signed short s_s;
+signed short int s_si;
+
+unsigned short u_s;
+unsigned short int u_si;
+
+int i;
+signed s;
+signed int si;
+
+unsigned u;
+unsigned int ui;
+
+long long ll;
+long long int lli;
+signed long long sll;
+signed long long int slli;
+
+unsigned long long ull;
+unsigned long long int ulli;
+
+float f;
+
+double d;
+long double ld;
+
+_Bool b;
+
+float _Complex fc;
+double _Complex dc;
+long double _Complex ldc;
+
+float _Imaginary fi;
+double _Imaginary di;
+long double _Imaginary ldi;
