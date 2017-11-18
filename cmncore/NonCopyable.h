aca4ba1e682f99c9e7f2ba0fad480fd70bfd3f6e
yorickdewid <yorick17@outlook.com>
2017-11-18 20:36:04 +0100

    Add common source target

---
+++ cmncore/NonCopyable.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2017 Quenza Inc. All rights reserved.
+//
+// This file is part of the Cryptox project.
+//
+// Use of this source code is governed by a private license
+// that can be found in the LICENSE file. Content can not be 
+// copied and/or distributed without the express of the author.
+
+#pragma once
+
+namespace CmnCore
+{
+
+struct NonCopyable
+{
+	NonCopyable() = default;
+	NonCopyable(const NonCopyable&) = delete;
+	NonCopyable(NonCopyable&&) = delete;
+	NonCopyable& operator=(const NonCopyable&) = delete;
+	NonCopyable& operator=(NonCopyable&&) = delete;
+};
+
+} // namespace CmnCore
