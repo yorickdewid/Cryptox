3d571389f31e6f2546a81516528181cbd85bb945
yorickdewid <yorick17@outlook.com>
2017-12-03 00:20:22 +0100

    Make AST state iteratable

--- coilcl/src/ASTState.h
+++ coilcl/src/ASTState.h
@@ -21,16 +21,42 @@ std::unique_ptr<_ToTy> static_unique_pointer_cast(std::unique_ptr<_FromTy>&& old
 	return std::unique_ptr<_ToTy>{ static_cast<_ToTy*>(old.release()) };
 }
 
-template<typename _BaseTy>
+template<typename _BaseTy, typename _Container = std::deque<std::shared_ptr<_BaseTy>>>
 class ASTState
 {
 public:
-	ASTState()
-	{
-	}
+	using container_type = _Container;
+	using allocator_type = typename _Container::allocator_type;
+	using value_type = typename _Container::value_type;
+	using size_type = typename _Container::size_type;
+	using difference_type = typename _Container::difference_type;
+	using reference = typename _Container::reference;
+	using const_reference = typename _Container::const_reference;
+	using pointer = typename _Container::pointer;
+	using const_pointer = typename _Container::const_pointer;
+
+public:
+	using iterator = typename _Container::iterator;
+	using const_iterator = typename _Container::const_iterator;
+	using reverse_iterator = typename _Container::reverse_iterator;
+	using const_reverse_iterator = typename _Container::const_reverse_iterator;
+
+public:
+	ASTState() = default;
+
+	iterator begin() noexcept { return m_mementoList.begin(); }
+	const_iterator begin() const noexcept { return m_mementoList.cbegin(); }
+	iterator end() noexcept { return m_mementoList.end(); }
+	const_iterator end() const noexcept { return m_mementoList.cend(); }
+
+	reference front() { return m_mementoList.front(); }
+	const_reference front() const { return m_mementoList.front(); }
+	reference back() { return m_mementoList.back(); }
+	const_reference back() const { return m_mementoList.back(); }
 
 	// friend?
-	auto Alteration() const { return m_mementoList.size(); };
+	inline auto Alteration() const { return m_mementoList.size(); };
+	inline auto HasAlteration() const { return !m_mementoList.empty(); };
 
 	template<typename _Ty>
 	void Bump(_Ty objectCpyState)
@@ -39,11 +65,11 @@ public:
 
 		auto ptrBase = static_unique_pointer_cast<_BaseTy>(std::move(origPtr));
 
-		m_mementoList.push(std::move(ptrBase));
+		m_mementoList.push_back(std::move(ptrBase));
 	}
 
 private:
-	std::stack<std::shared_ptr<_BaseTy>> m_mementoList;
+	_Container m_mementoList;
 };
 
 } // namespace AST
