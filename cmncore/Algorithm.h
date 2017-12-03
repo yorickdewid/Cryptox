a66e323a5184851987a68eaf19e242a221b751bd
yorickdewid <yorick17@outlook.com>
2017-12-03 13:58:28 +0100

    Algorithm library

---
+++ cmncore/Algorithm.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2017 Quenza Inc. All rights reserved.
+//
+// This file is part of the Cryptox project.
+//
+// Use of this source code is governed by a private license
+// that can be found in the LICENSE file. Content can not be 
+// copied and/or distributed without the express of the author.
+
+#pragma once
+
+#include <stdexcept>
+
+namespace CmnCore
+{
+namespace Algorithm
+{
+
+template<typename _InputIt, typename _UnaryPredicate, typename _UnaryCallback>
+void OnMatch(_InputIt first, _InputIt last, _UnaryPredicate p, _UnaryCallback c)
+{
+	for (; first != last; ++first) {
+		if (p(*first)) {
+			c(first);
+		}
+	}
+}
+
+} // namespace Except
+} // namespace CmnCore
