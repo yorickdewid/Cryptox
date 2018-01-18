d11ce0fc873a9cc2b346fcd21f6dc5fdd73d039a
yorickdewid <yorick17@outlook.com>
2018-01-18 22:57:30 +0100

    Default style

---
+++ contrib/Style.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2017 Quenza Inc. All rights reserved.
+// Copyright (c) 2018 Blub Corp. All rights reserved.
+//
+// This file is part of the Cryptox project.
+//
+// Use of this source code is governed by a private license
+// that can be found in the LICENSE file. Content can not be 
+// copied and/or distributed without the express of the author.
+
+namespace DefaultNamespace
+{
+
+namespace Subspace
+{
+
+template<typename _Ty>
+int helper_call()
+{
+    const auto& ptr = std::make_shared<_Ty>(12, "test");
+    ptr->Initialize();
+    return ptr->NativeAPICode(); 
+}
+    
+} // namespace Subspace
+
+class SomeClass
+{
+    int m_size;
+    size_t m_offset;
+
+public:
+    inline void SetLenth(int sz) { m_size = sz; }
+    inline void SetOffset(size_t offset) { m_offset = offset; }
+
+};
+
+} // namespace DefaultNamespace
