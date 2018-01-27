e3f2b768afceb10c8bed396e44ad97ec3b9478b6
yorickdewid <yorick17@outlook.com>
2018-01-27 23:49:54 +0100

    Tokenizer interface

---
+++ src/coilcl/src/Tokenizer.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2017 Quenza Inc. All rights reserved.
+//
+// This file is part of the Cryptox project.
+//
+// Use of this source code is governed by a private license
+// that can be found in the LICENSE file. Content can not be 
+// copied and/or distributed without the express of the author.
+
+#pragma once
+
+namespace CoilCl
+{
+
+// Tokenizer interface used by subsequent stages. Next stags
+// and token consumers must interact with the methods directed
+// by this interface.
+struct Tokenizer
+{
+	using ErrorHandler = std::function<void(const std::string& msg, char token, int line, int column)>;
+	
+	// Register error handler
+	virtual void ErrorHandler(const ErrorHandler errHandler) { errHandlerFunc = errHandler; }
+
+	// Return whether or not the current state yields data
+	virtual bool HasData() const = 0;
+
+	// Return if tokenizer is done
+	virtual bool IsDone() const = 0;
+
+	// Current token line
+	virtual int TokenLine() const = 0;
+
+	// Current token column
+	virtual int TokenColumn() const = 0;
+
+	// Move tokenizer into next machine state
+	virtual int Lex() = 0;
+
+protected:
+	ErrorHandler errHandlerFunc;
+};
+
+using TokenizerPtr = std::shared_ptr<Tokenizer>;
+
+} // namespace CoilCl
