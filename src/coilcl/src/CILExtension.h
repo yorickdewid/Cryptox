c466c7d2ab526aeb43a1428f3e820bf2e413fff6
yorickdewid <yorick17@outlook.com>
2018-02-03 23:22:56 +0100

    Prep new sources

---
+++ src/coilcl/src/CILExtension.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 Quenza Inc. All rights reserved.
+//
+// This file is part of the Cryptox project.
+//
+// Use of this source code is governed by a private license
+// that can be found in the LICENSE file. Content can not be
+// copied and/or distributed without the express of the author.
+
+#pragma once
+
+namespace CoilCl
+{
+
+struct LanguageExtension
+{
+	///
+};
+
+class CryIntermediate : public LanguageExtension
+{
+public:
+	CryIntermediate();
+};
+
+} // namespace CoilCl
