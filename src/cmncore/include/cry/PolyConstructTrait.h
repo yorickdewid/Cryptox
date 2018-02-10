d51b82f5a04d9b0ea69e00d0d5f09cb70a44f051
yorickdewid <yorick17@outlook.com>
2018-02-10 22:44:48 +0100

    Polymorphic copy and construct contract

---
+++ src/cmncore/include/cry/PolyConstructTrait.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2017 Quenza Inc. All rights reserved.
+//
+// This file is part of the Cryptox project.
+//
+// Use of this source code is governed by a private license
+// that can be found in the LICENSE file. Content can not be 
+// copied and/or distributed without the express of the author.
+
+#pragma once
+
+#include "Cry.h"
+
+namespace Cry
+{
+
+// Polyconstruct contract demands the implementing class
+// to provide creation and cloning methods so that if the
+// class is abstract and polymorphic, the callee can still
+// request copies of the object.
+struct PolyConstruct
+{
+	virtual PolyConstruct *Construct() const = 0;
+	virtual PolyConstruct *Copy() const = 0;
+	virtual ~PolyConstruct() {}
+};
+
+} // namespace Cry
