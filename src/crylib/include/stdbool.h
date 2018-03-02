babe36f7cba9052d3f73f42898be093ab5f65b74
yorickdewid <yorick17@outlook.com>
2018-03-02 14:41:45 +0100

    errno.h & stdbool.h

---
+++ src/crylib/include/stdbool.h
@@ -0,0 +1,20 @@
+/**
+* Copyright (C) 2018 Cryptox, Blub Corp.
+* All Rights Reserved
+*
+* This file is part of the Cryptox project.
+*
+* Content can not be copied and/or distributed without the express
+* permission of the author.
+*/
+
+#pragma once
+
+#define bool  _Bool
+#define true  1
+#define false 0
+
+#define TRUE  1
+#define FALSE 0
+
+#define __bool_true_false_defined 1
