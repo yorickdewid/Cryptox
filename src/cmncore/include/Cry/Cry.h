211530d22bb37cc6eb86a269f824392cf8384205
yorickdewid <yorick17@outlook.com>
2018-03-12 00:36:27 +0100

    Indentify OS

--- src/cmncore/include/Cry/Cry.h
+++ src/cmncore/include/Cry/Cry.h
@@ -10,3 +10,31 @@
 
 #define CSTD std::
 #define CNOEXCEPT noexcept
+
+#ifdef _WIN32
+# define CRY_WINDOWS 1
+#ifdef _WIN64
+# define CRY_WINDOWS64 1
+#else
+# define CRY_WINDOWS32 1
+#endif
+#elif __APPLE__
+# include "TargetConditionals.h"
+# if TARGET_IPHONE_SIMULATOR
+#  define CRY_IOS_SIM 1
+# elif TARGET_OS_IPHONE
+#  define CRY_IOS 1
+# elif TARGET_OS_MAC
+#  define CRY_OSX 1
+# else
+#  error "Unknown Apple platform"
+# endif
+#elif __linux__
+# define CRY_LINUX 1
+#elif __unix__
+# define CRY_UNIX 1
+#elif defined(_POSIX_VERSION)
+# define CRY_POSIX 1
+#else
+# error "Unknown compiler"
+#endif
