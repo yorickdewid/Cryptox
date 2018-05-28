c1526e5ac104f9052009e23ff5a7bef18832354d
yorickdewid <yorick17@outlook.com>
2018-05-28 22:00:09 +0200

    Configure source generation to use the event message generator, and provide a local backup

---
+++ cmake/FindGit.cmake
@@ -0,0 +1,13 @@
+# Copyright (c) 2017 Quenza Inc. All rights reserved.
+# Copyright (c) 2018 Blub Corp. All rights reserved.
+#
+# This file is part of the Cryptox project.
+#
+# Use of this source code is governed by a private license
+# that can be found in the LICENSE file. Content can not be 
+# copied and/or distributed without the express of the author.
+
+find_program(GIT_EXE NAMES git)
+if(NOT GIT_EXE)
+    message(FATAL_ERROR "Git not found")
+endif()
---
+++ cmake/FindMsgGen.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2017 Quenza Inc. All rights reserved.
+# Copyright (c) 2018 Blub Corp. All rights reserved.
+#
+# This file is part of the Cryptox project.
+#
+# Use of this source code is governed by a private license
+# that can be found in the LICENSE file. Content can not be 
+# copied and/or distributed without the express of the author.
+
+find_program(MSGGEN_EXE msggen
+    PATHS
+        ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/*
+        ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/*
+)
+if(NOT MSGGEN_EXE)
+    find_program(MSGGEN_EXE msggen
+        NAMES msggen_x64
+        PATHS ${CONTRIB_DIR}
+    )
+    if(NOT MSGGEN_EXE)
+        message(FATAL_ERROR "Generator not found")
+    endif()
+    message(STATUS "Using contrib event generator")
+endif()
